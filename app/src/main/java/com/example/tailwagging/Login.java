107b11877c89c8e77dc96dcf6910ff5625e6f87f
Md. Sadik Mahmud Adive <siradive137@gmail.com>
2025-03-29 03:16:12 +0600

    updated

--- app/src/main/java/com/example/tailwagging/Login.java
+++ app/src/main/java/com/example/tailwagging/Login.java
@@ -1,9 +1,6 @@
 package com.example.tailwagging;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class OnbordingScreen1 extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_onbording_screen1);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button btnSetStarted = findViewById(R.id.btnGetStarted);
-
-        btnSetStarted.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(OnbordingScreen1.this, Login.class);
-                startActivity(intent);
-            }
         });
     }
 }
